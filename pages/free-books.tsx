fb618d6a2b343c74c7e3dc55d31985802bd9f412
JGHB <33645877+JGHB@users.noreply.github.com>
2023-02-01 23:40:30 -0500

    put Project Gutenberg api calls in getStaticProps and fixed minor styling bugs

--- pages/free-books.tsx
+++ pages/free-books.tsx
@@ -3,7 +3,45 @@ import styles from '@/styles/FreeBooks.module.css'
 import React, {useState} from "react";
 import Book from "@/components/book/book";
 
-export default function FreeBooks() {
+
+export async function getStaticProps() {
+
+    const mathematicsData = await fetch(`https://gutendex.com/books/?topic=mathematics`)
+        .then((response) => response.json())
+    const biologyData = await fetch(`https://gutendex.com/books/?topic=biology`)
+        .then((response) => response.json())
+    const physicsData = await fetch(`https://gutendex.com/books/?topic=physics`)
+        .then((response) => response.json())
+    const chemistryData = await fetch(`https://gutendex.com/books/?topic=chemistry`)
+        .then((response) => response.json())
+    const physiologyData = await fetch(`https://gutendex.com/books/?topic=biology`)
+        .then((response) => response.json())
+    const microBiologyData = await fetch(`https://gutendex.com/books/?topic=microbiology`)
+        .then((response) => response.json())
+
+    const allPostsData = {
+        mathematicsData,
+        biologyData,
+        physicsData,
+        chemistryData,
+        physiologyData,
+        microBiologyData
+    }
+
+    console.log(allPostsData)
+
+    return {
+        props: {
+            allPostsData,
+        },
+    };
+}
+
+type postsProps = {
+    allPostsData: any
+}
+
+export default function FreeBooks({allPostsData}: postsProps) {
 
     interface BookData {
         id: number;
@@ -17,25 +55,63 @@ export default function FreeBooks() {
     const [currentSubject, setCurrentSubject] = useState<string>('choose')
     const [books, setBooks] = useState<BookData[] | null>(null)
 
-    const updateBooks = async () => {
-        if (currentSubject === 'choose') return
-        const data = await fetch(`https://gutendex.com/books/?topic=${currentSubject}`)
-            .then((response) => response.json())
-        const newBooks = data.results.map((book: any) => {
+    const filterBookData = (unfilteredBookData: any) => {
+        return unfilteredBookData.results.map((book: any) => {
             return {
-                id: book.id,
-                title: book.title,
-                author: book.authors.length > 0 ? book.authors[0].name : null,
-                imageURL: book.formats['image/jpeg'] ? book.formats['image/jpeg'] : undefined,
-                epubURL: book.formats['application/epub+zip']
+                id: book?.id,
+                title: book?.title,
+                author: book?.authors?.length > 0 ? book.authors[0].name : null,
+                imageURL: book?.formats['image/jpeg'] ? book.formats['image/jpeg'] : undefined,
+                epubURL: book?.formats['application/epub+zip']
                     ? book.formats['application/epub+zip'] : null,
-                pdfURL: book.formats['application/pdf'] ? book.formats['application/pdf'] : null
+                pdfURL: book?.formats['application/pdf'] ? book.formats['application/pdf'] : null
             }
         })
-        setBooks(newBooks)
+    }
+
+    const filteredMathematicsBooks = filterBookData(allPostsData.mathematicsData)
+    const filteredBiologyBooks = filterBookData(allPostsData.biologyData)
+    const filteredPhysicsBooks = filterBookData(allPostsData.physicsData)
+    const filteredChemistryBooks = filterBookData(allPostsData.chemistryData)
+    const filteredPhysiologyBooks = filterBookData(allPostsData.physiologyData)
+    const filteredMicroBiologyBooks = filterBookData(allPostsData.microBiologyData)
+
+
+    const updateBooks = async () => {
+        if (currentSubject === 'choose') return
+        console.log("The Current Subject is " + currentSubject)
+        switch(currentSubject) {
+            case 'mathematics': {
+                setBooks(filteredMathematicsBooks)
+                break
+            }
+            case 'biology': {
+                setBooks(filteredBiologyBooks)
+                break
+            }
+            case 'physics': {
+                setBooks(filteredPhysicsBooks)
+                break
+            }
+            case 'chemistry': {
+                setBooks(filteredChemistryBooks)
+                break
+            }
+            case 'physiology': {
+                setBooks(filteredPhysiologyBooks)
+                break
+            }
+            case 'microbiology': {
+                setBooks(filteredMicroBiologyBooks)
+                break
+            }
+            default:
+                return
+        }
     }
 
     const handleSubjectChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+        console.log(e.target.value)
         setCurrentSubject(e.target.value);
     };
 
